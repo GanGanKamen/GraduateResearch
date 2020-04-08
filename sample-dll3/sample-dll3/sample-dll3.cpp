614549bdf6a5bd63d5d1378fb945a39346c04c96
GanGanKamen <m01171408c@edu.teu.ac.jp>
2020-04-08 14:32:49 +0900

    試しにdllプロジェクトを作成

---
+++ sample-dll3/sample-dll3/sample-dll3.cpp
@@ -0,0 +1,28 @@
+﻿// sample-dll3.cpp : DLL 用にエクスポートされる関数を定義します。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "sample-dll3.h"
+
+
+// これは、エクスポートされた変数の例です
+SAMPLEDLL3_API int nsampledll3=0;
+
+// これは、エクスポートされた関数の例です。
+SAMPLEDLL3_API int fnsampledll3(void)
+{
+    return 0;
+}
+
+int counter = 0;
+
+SAMPLEDLL3_API int CountUp(void) {
+	return counter++;
+}
+
+// これは、エクスポートされたクラスのコンストラクターです。
+Csampledll3::Csampledll3()
+{
+    return;
+}
