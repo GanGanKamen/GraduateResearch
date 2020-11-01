73924000dc7befc2ac3a874f70dfcba7d5758d74
GanGanKamen <m01171408c@edu.teu.ac.jp>
2020-11-01 11:20:39 +0900

    MPU6050で回転可能

--- Vibration Project/Assets/Scripts/Test/TestGyiroSerial.cs
+++ Vibration Project/Assets/Scripts/Test/TestGyiroSerial.cs
@@ -6,27 +6,23 @@ using UnityEngine;
 public class TestGyiroSerial : MonoBehaviour
 {
     [SerializeField] private Transform transform;
-    [SerializeField] private Vector2 range;
-    [SerializeField] private float rotX; private float preX;
-    [SerializeField] private float rotY; private float preY;
-    [SerializeField] private float rotZ; private float preZ;
+    [SerializeField] private float speed;
+    [SerializeField] private float input;
+    private float[] paramaters;
+    private float[] preParas;
 
-    [SerializeField] private float inputX;
-    [SerializeField] private float inputY;
-    [SerializeField] private float inputZ;
-
-    private bool isInit = false;
     private float count = 0;
-
-    private float defRotX = -6f;
+    private bool isInit = false;
     void Start()
     {
-
+        paramaters = new float[6];
+        preParas = new float[6];
     }
 
     // Update is called once per frame
     void Update()
     {
+        input = paramaters[4];
         if (isInit == false)
         {
             count += Time.deltaTime;
@@ -38,95 +34,32 @@ public class TestGyiroSerial : MonoBehaviour
         }
         else
         {
-            
+            if (paramaters[4] <= -10) transform.eulerAngles += new Vector3(0, Time.deltaTime * speed, 0);
+            if(paramaters[4] >= 10) transform.eulerAngles -= new Vector3(0, Time.deltaTime * speed, 0);
+            /*
+            var diffrence = Mathf.Abs(paramaters[4] - preParas[4]);
+            if (paramaters[4] >= 20)
+            {
+                if (paramaters[4] > preParas[4]) Debug.Log("1");
+                else Debug.Log("-1");
+            }
+            preParas[4] = paramaters[4];
+            */
         }
     }
 
     public void ReadComplateString(object data)
     {
+        
         var text = data as string;
         string[] arr = text.Split(',');
 
-        if (arr.Length < 3) return;
-        bool canRoll = float.TryParse(arr[0], out rotX);
-        if (canRoll) rotX = float.Parse(arr[0]);
-        bool canPitch = float.TryParse(arr[1], out rotY);
-        if (canPitch) rotY = float.Parse(arr[1]);
-        bool canYaw = float.TryParse(arr[2], out rotZ);
-        if (canYaw) rotZ = float.Parse(arr[2]);
-
-        switch (isInit)
+        if (arr.Length < 6) return;
+        for(int i = 0; i< 6; i++)
         {
-            case false:
-                break;
-            case true:
-                if (rotX != preX) ;
-                var delta = rotX - preX;
-                transform.eulerAngles += new Vector3(0, delta, 0);
-                /*
-                var rotDeltaX = Mathf.Abs(rotX - defRotX);
-                if (rotDeltaX > range.x)
-                {
-                    if (rotDeltaX > range.y) rotDeltaX = range.y;
-                    bool trigger = rotX > defRotX;
-                    switch (trigger)
-                    {
-                        case true:
-                            var val = rotX - defRotX;
-                            inputX = Map(val,range.x + defRotX,range.y + defRotX,0,1);
-                            if (inputX < 0.1f) inputX = 0;
-                            break;
-                        case false:
-                            //var val1 = defRotX- rotX;
-                            //inputX = Map(val1, range.x - defRotX, range.y - defRotX, -1, 0);
-                            //if (inputX > -0.1f) inputX = 0;
-                            break;
-                    }
-                }
-                
-                var rotDeltaX = Mathf.Abs(rotX - preX);
-                if (rotDeltaX > range.x)
-                {
-                    if (rotX > range.y) rotDeltaX = range.y;
-                    bool trigger = rotDeltaX > preX;
-                    switch (trigger)
-                    {
-                        case true:
-                            inputX = Map(rotDeltaX - preX, range.x - preX, range.y + preX, 0, 1);
-                            if (inputX < 0.1f) inputX = 0;
-                            break;
-                        case false:
-                            inputX = Map(preX - rotDeltaX, preX - range.y, preX - range.x, -1, 0);
-                            if (inputX > -0.1f) inputX = 0;
-                            break;
-                    }
-                }
-                else preX = rotX;
-                
-                var rotDeltaY = Mathf.Abs(rotY - preY);
-                if (rotDeltaY > range.x)
-                {
-                    if (rotDeltaY > range.y) rotDeltaY = range.y;
-                    bool trigger = rotDeltaY > preY;
-                    switch (trigger)
-                    {
-                        case true:
-                            inputY = Map(rotDeltaX - preY, range.x - preY, range.y + preY, 0, 1);
-                            break;
-                        case false:
-                            inputY = Map(preY - rotDeltaX, preY - range.y, preY - range.x, -1, 0);
-                            break;
-                    }
-                }
-                else preY = rotY;
-                */
-
-
-                break;
-        }
-        preX = rotX;
-        preY = rotY;
-        preZ = rotZ;
+            var canPerse = float.TryParse(arr[i], out paramaters[i]);
+            if (canPerse) paramaters[i] = float.Parse(arr[i]);          
+        } 
         
     }
 
