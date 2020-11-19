d25f64ef51e2aa0206ecd578f3ae8933ba9d9252
GanGanKamen <m01171408c@edu.teu.ac.jp>
2020-11-19 11:59:25 +0900

    アプリ終了時

--- Vibration Project/Assets/Scripts/Primitive/VestManager.cs
+++ Vibration Project/Assets/Scripts/Primitive/VestManager.cs
@@ -288,7 +288,14 @@ public class VestManager : MonoBehaviour
         yield break;
     }
 
-    float Map(float value, float start1, float stop1, float start2, float stop2)
+    void OnApplicationQuit()
+    {
+        StopAllBlood();
+        StopAllHit();
+        SerialPort.Write("z");
+    }
+
+    private float Map(float value, float start1, float stop1, float start2, float stop2)
     {
         return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
     }
